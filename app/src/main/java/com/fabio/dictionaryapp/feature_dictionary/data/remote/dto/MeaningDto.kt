2180859518978140733f5bffb9fafa5406e50bc6
Fabio Insolia <fabio.insolia@gmail.com>
2022-08-09 15:42:53 +0100

    Created mapper from Dto to domain models

--- app/src/main/java/com/fabio/dictionaryapp/feature_dictionary/data/remote/dto/MeaningDto.kt
+++ app/src/main/java/com/fabio/dictionaryapp/feature_dictionary/data/remote/dto/MeaningDto.kt
@@ -1,8 +1,15 @@
 package com.fabio.dictionaryapp.feature_dictionary.data.remote.dto
 
+import com.fabio.dictionaryapp.feature_dictionary.domain.model.Meaning
+
 data class MeaningDto(
-    val antonyms: List<Any>,
     val definitions: List<DefinitionDto>,
     val partOfSpeech: String,
-    val synonyms: List<String>
-)
+) {
+    fun toMeaning(): Meaning {
+        return Meaning(
+            definitions = definitions.map { it.toDefinition() },
+            partOfSpeech = partOfSpeech
+        )
+    }
+}
--- app/src/main/java/com/fabio/dictionaryapp/feature_dictionary/data/remote/dto/WordInfoDto.kt
+++ app/src/main/java/com/fabio/dictionaryapp/feature_dictionary/data/remote/dto/WordInfoDto.kt
@@ -1,10 +1,20 @@
 package com.fabio.dictionaryapp.feature_dictionary.data.remote.dto
 
+import com.fabio.dictionaryapp.feature_dictionary.domain.model.WordInfo
+
 data class WordInfoDto(
-    val license: License,
     val meanings: List<MeaningDto>,
+    val origin: String,
     val phonetic: String,
     val phonetics: List<PhoneticDto>,
-    val sourceUrls: List<String>,
     val word: String
-)
+) {
+    fun toWordInfo(): WordInfo {
+        return WordInfo(
+            meanings = meanings.map { it.toMeaning() },
+            origin = origin,
+            phonetic = phonetic,
+            word = word
+        )
+    }
+}
