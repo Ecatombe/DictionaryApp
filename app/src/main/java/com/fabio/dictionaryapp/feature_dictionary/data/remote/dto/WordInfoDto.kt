936a43a45cae21132e8c53ceb2e428bdca6160cd
Fabio Insolia <fabio.insolia@gmail.com>
2022-08-09 15:16:51 +0100

    Basic gradle import, remote dto and initial retrofit interface

---
+++ app/src/main/java/com/fabio/dictionaryapp/feature_dictionary/data/remote/dto/WordInfoDto.kt
@@ -0,0 +1,10 @@
+package com.fabio.dictionaryapp.feature_dictionary.data.remote.dto
+
+data class WordInfoDto(
+    val license: License,
+    val meanings: List<MeaningDto>,
+    val phonetic: String,
+    val phonetics: List<PhoneticDto>,
+    val sourceUrls: List<String>,
+    val word: String
+)
